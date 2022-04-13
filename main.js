1b9daa30bf26d3d4092570c86f68557e65bea422
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-04-13 10:40:13 +0530

    :fire: sign in complete

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const puppet = require("puppeteer");
-const credential = [`jatinbishtcodes2308@gmail.com`,`jatinbisht2308`];
+const credential = [`jatinbishtcodes2308@gmail.com`,`jatin2308`];
 let browserOpenPromise = puppet.launch({
     headless: false,
     defaultViewport: false,
@@ -29,14 +29,29 @@ let browserOpenPromise = puppet.launch({
   .then (function () {
       console.log("Clicked on login button!!!");
     //   Now its time to type inside the user name 
-    let userNameTypedPromise = currentPage.type("input[id='username']",credential[0], {delay: 100});
+    let userNameTypedPromise = currentPage.type("input[id='username']",credential[0], {delay: 300});
     return userNameTypedPromise;
   }).then (function () {
       console.log("Username is typed!!!!");
     //   Now its time to type the password
-    let passwordIsTypedPromise = currentPage.type("input[id='password']",credential[1],{delay:50});
+    let passwordIsTypedPromise = currentPage.type("input[id='password']",credential[1],{delay:200});
     return passwordIsTypedPromise;
   })
   .then(function(){
       console.log("Password is typed!!!!!");
+    //   Now its time to click on sign button
+    setTimeout(function () {},2000);
+    let clickOnSignPromise = currentPage.click("button[class='btn__primary--large from__button--floating']");
+    return clickOnSignPromise;
   })
+  .then(function (){
+      console.log("Successfully signed in linkedin!!!!");
+    //   setTimeout(function () {},6000);
+    //   Now lets move to your profile page
+    let movedToProfilePagePromise = currentPage.click("a[id='ember605']");
+    return movedToProfilePagePromise;
+  })
+//   .then (function () {
+//       console.log("Moved to profile page succesfully!!!!!!");
+
+//   })
